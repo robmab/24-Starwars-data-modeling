be1e6f8a05a403666a82968d90842310be54b011
robmab <rob_mb@outlook.es>
2023-05-17 18:03:49 +0200

    plantilla

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 class Person(Base):
-    __tablename__ = 'prueba2'
+    __tablename__ = 'otraprueba'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True) 
